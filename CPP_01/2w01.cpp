09344ffa329884c14bc588a55f505394c5760dbe
danno <danno@>
2022-09-13 20:30:21 +0900

    초기 커밋.

---
+++ CPP_01/2w01.cpp
@@ -0,0 +1,44 @@
+//20215124 김다은 01
+#include <iostream>
+using namespace std;
+
+int main() {
+	char eng;
+	int cons = 0;	// 자음
+	int vowel = 0;	//모음
+
+
+	while (1) {
+		cout << "영문자를 입력하고 ctrl+z를 치세요" << endl;
+		cin >> eng;
+
+		if (cin.eof()) {
+			cout << "모음: " << vowel << endl;
+			cout << "자음: " << cons << endl;
+			break;
+		}
+
+		switch (eng) {
+		case 'a':
+		case 'e':
+		case 'i':
+		case 'o':
+		case 'u':
+			vowel++;
+			break;
+		case 'A':
+		case 'E':
+		case 'I':
+		case 'O':
+		case 'U':
+			[[fallthrough]];
+			cout << "check to alphabet" << endl;
+			break;
+		default:
+			cons++;
+			break;
+		}
+	}
+
+	return 0;
+}
