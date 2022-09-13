29a115c72a6d71ebba839fc270284b052cd9d280
danno <danno33@naver.com>
2022-09-13 21:00:57 +0900

    04 추가

---
+++ CPP_01/2w04.cpp
@@ -0,0 +1,24 @@
+//20215124 04
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num, i = 1;
+	int sum = 0;
+
+	cout << "정수를 입력하세요: ";
+	cin >> num;
+	
+	cout << i;
+	for (i = 2; i < num; i++) {
+		if (num % i == 0) {
+			cout << " + " << i;
+			sum += i;
+		}
+		else
+			continue;
+	}
+	cout << " = " << sum << endl;
+
+	return 0;
+}
